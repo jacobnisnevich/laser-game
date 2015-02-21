ccacc5ef4b0586ee725c3cd30ef0ab85e4f6ebc4
jacobnisnevich <jacob.nisnevich@gmail.com>
2015-02-20 20:11:09 -0800

    swappable tiles

--- js/levels.js
+++ js/levels.js
@@ -2,7 +2,7 @@ var levelOne = [
 		['corner-left-right','wall','wall','wall','wall','wall','corner-right-left'],
 		['wall','floor','floor','floor','floor','floor','wall'],
 		['wall','floor','floor','floor','floor','floor','wall'],
-		['laser-red-right','floor','floor','floor','floor','floor','wall'],
+		['laser-red-right','floor','mirror-left-right','floor','floor','floor','wall'],
 		['wall','floor','floor','floor','floor','floor','wall'],
 		['wall','floor','floor','floor','floor','floor','wall'],
 		['corner-right-left','wall','wall','wall','wall','wall','corner-left-right']
