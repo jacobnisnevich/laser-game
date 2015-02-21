b0c3873778351f5f39e5b269d34344502742e969
jacobnisnevich <jacob.nisnevich@gmail.com>
2015-02-20 17:19:47 -0800

    added loadLevelData function

---
+++ js/levels.js
@@ -0,0 +1,9 @@
+var levelOne = [
+		['corner-left-right','wall','wall','wall','wall','wall','corner-right-left'],
+		['wall','floor','floor','floor','floor','floor','wall'],
+		['wall','floor','floor','floor','floor','floor','wall'],
+		['laser-red-right','floor','floor','floor','floor','floor','wall'],
+		['wall','floor','floor','floor','floor','floor','wall'],
+		['wall','floor','floor','floor','floor','floor','wall'],
+		['corner-right-left','wall','wall','wall','wall','wall','corner-left-right']
+	];
