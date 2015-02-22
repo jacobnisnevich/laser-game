d6c8446afcae064e9570f7b6e7db5810fcd02e40
Jason Yang <JasonYang96@ucla.edu>
2015-02-22 12:42:20 -0800

    added createGridContainer() function

--- js/levels.js
+++ js/levels.js
@@ -1,9 +1,10 @@
 var levelOne = [
-		['corner-left-right','wall','wall','wall','wall','wall','corner-right-left'],
-		['wall','floor','floor','floor','floor','floor','wall'],
-		['wall','floor','floor','floor','floor','floor','wall'],
-		['laser-red-right','floor','mirror-left-right','floor','floor','floor','wall'],
-		['wall','floor','floor','floor','floor','floor','wall'],
-		['wall','floor','floor','floor','floor','floor','wall'],
-		['corner-right-left','wall','wall','hole-red-up','wall','wall','corner-left-right']
+		rcNum: 7;
+		grid:['corner-left-right','wall','wall','wall','wall','wall','corner-right-left'],
+			 ['wall','floor','floor','floor','floor','floor','wall'],
+			 ['wall','floor','floor','floor','floor','floor','wall'],
+			 ['laser-red-right','floor','mirror-left-right','floor','floor','floor','wall'],
+			 ['wall','floor','floor','floor','floor','floor','wall'],
+			 ['wall','floor','floor','floor','floor','floor','wall'],
+			 ['corner-right-left','wall','wall','hole-red-up','wall','wall','corner-left-right']
 	];
