d748fc19d8c0ed31b358d54f8912dfa9dc2a42e9
jacobnisnevich <jacob.nisnevich@gmail.com>
2015-03-05 01:57:14 -0800

    green laser sprites and next level works

--- js/levels.js
+++ js/levels.js
@@ -9,7 +9,7 @@ var levels = [ {
 			 ['wall','floor','floor','floor','floor','floor','wall'],
 			 ['corner-right-left','wall','wall','hole-red-up','wall','wall','corner-left-right']]
 	}, {
-		colors: ['red, green'],
+		colors: ['red', 'green'],
 		rcNum: 7,
 		grid:[['corner-left-right','wall','wall','wall','wall','wall','corner-right-left'],
 			 ['wall','floor','floor','floor','floor','floor','wall'],
